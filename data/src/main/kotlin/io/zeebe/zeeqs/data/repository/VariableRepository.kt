c7dfb9e2abe7dc13c25798e82495f0903ea5ef85
Philipp Ossler <philipp.ossler@gmail.com>
2021-03-09 14:45:03 +0100

    fix(data): add missing @Transactional for loading large objects
    * avoid database exception when loading large objects (LOBs)

--- data/src/main/kotlin/io/zeebe/zeeqs/data/repository/VariableRepository.kt
+++ data/src/main/kotlin/io/zeebe/zeeqs/data/repository/VariableRepository.kt
@@ -3,9 +3,11 @@ package io.zeebe.zeeqs.data.repository
 import io.zeebe.zeeqs.data.entity.Variable
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface VariableRepository : PagingAndSortingRepository<Variable, Long> {
 
+    @Transactional(readOnly = true)
     fun findByWorkflowInstanceKey(workflowInstanceKey: Long): List<Variable>
 }
