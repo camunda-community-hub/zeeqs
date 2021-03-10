901a68b19c20962e866f185c010c05f99df8ae91
Philipp Ossler <philipp.ossler@gmail.com>
2021-03-10 12:55:52 +0100

    feat(data): import and query errors (#107)
    * import error records and expose them as query

---
+++ data/src/main/kotlin/io/zeebe/zeeqs/data/entity/Error.kt
@@ -0,0 +1,13 @@
+package io.zeebe.zeeqs.data.entity
+
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Lob
+
+@Entity
+class Error(@Id val position: Long,
+            val errorEventPosition: Long,
+            @Lob val exceptionMessage: String,
+            @Lob val stacktrace:String,
+            val workflowInstanceKey: Long?
+            )
