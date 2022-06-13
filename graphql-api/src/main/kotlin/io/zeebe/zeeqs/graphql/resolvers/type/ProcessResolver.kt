e96bd00e7830e37805da2fa84f9d04eba134b4da
Philipp Ossler <philipp.ossler@gmail.com>
2022-06-13 15:44:03 +0200

    feat: find process element by id (#253)

--- graphql-api/src/main/kotlin/io/zeebe/zeeqs/graphql/resolvers/type/ProcessResolver.kt
+++ graphql-api/src/main/kotlin/io/zeebe/zeeqs/graphql/resolvers/type/ProcessResolver.kt
@@ -8,6 +8,7 @@ import io.zeebe.zeeqs.data.entity.ProcessInstanceState
 import io.zeebe.zeeqs.data.repository.MessageSubscriptionRepository
 import io.zeebe.zeeqs.data.repository.TimerRepository
 import io.zeebe.zeeqs.data.repository.ProcessInstanceRepository
+import io.zeebe.zeeqs.data.service.BpmnElementInfo
 import io.zeebe.zeeqs.data.service.ProcessService
 import io.zeebe.zeeqs.graphql.resolvers.connection.ProcessInstanceConnection
 import org.springframework.data.domain.PageRequest
@@ -15,10 +16,10 @@ import org.springframework.stereotype.Component
 
 @Component
 class ProcessResolver(
-    val processInstanceRepository: ProcessInstanceRepository,
-    val timerRepository: TimerRepository,
-    val messageSubscriptionRepository: MessageSubscriptionRepository,
-    val processService: ProcessService
+        val processInstanceRepository: ProcessInstanceRepository,
+        val timerRepository: TimerRepository,
+        val messageSubscriptionRepository: MessageSubscriptionRepository,
+        val processService: ProcessService
 ) : GraphQLResolver<Process> {
 
     fun processInstances(process: Process, perPage: Int, page: Int, stateIn: List<ProcessInstanceState>): ProcessInstanceConnection {
@@ -44,14 +45,22 @@ class ProcessResolver(
         return processService
                 .getBpmnElementInfo(process.key)
                 ?.values
-                ?.map {
-                    BpmnElement(
-                            processDefinitionKey = process.key,
-                            elementId = it.elementId,
-                            elementType = it.elementType
-                    )
-                }
+                ?.map { asBpmnElement(process, it) }
                 ?: emptyList()
+    }
+
+    private fun asBpmnElement(process: Process, it: BpmnElementInfo) =
+            BpmnElement(
+                    processDefinitionKey = process.key,
+                    elementId = it.elementId,
+                    elementType = it.elementType
+            )
+
+    fun element(process: Process, elementId: String): BpmnElement? {
+        return processService
+                .getBpmnElementInfo(process.key)
+                ?.get(elementId)
+                ?.let { asBpmnElement(process, it) }
     }
 
 }
