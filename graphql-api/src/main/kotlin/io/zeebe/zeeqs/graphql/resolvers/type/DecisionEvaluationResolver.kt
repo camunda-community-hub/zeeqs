0554f6ffd770f27039e817490319eb3feaea0cfd
Philipp Ossler <philipp.ossler@gmail.com>
2023-03-02 06:45:52 +0100

    feat: Get the evaluation time of the decision
    Add a field in the GraphQL API for decision evaluations to retrieve the time of the evaluation.

--- graphql-api/src/main/kotlin/io/zeebe/zeeqs/graphql/resolvers/type/DecisionEvaluationResolver.kt
+++ graphql-api/src/main/kotlin/io/zeebe/zeeqs/graphql/resolvers/type/DecisionEvaluationResolver.kt
@@ -3,6 +3,7 @@ package io.zeebe.zeeqs.graphql.resolvers.type
 import io.zeebe.zeeqs.data.entity.*
 import io.zeebe.zeeqs.data.repository.*
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
 import kotlin.jvm.optionals.getOrNull
@@ -43,6 +44,19 @@ class DecisionEvaluationResolver(
     fun elementInstance(decisionEvaluation: DecisionEvaluation): ElementInstance? {
         return decisionEvaluation.elementInstanceKey?.let {
             elementInstanceRepository.findByIdOrNull(it)
+        }
+    }
+
+    @SchemaMapping(typeName = "DecisionEvaluation", field = "evaluationTime")
+    fun evaluationTime(
+        decisionEvaluation: DecisionEvaluation,
+        @Argument zoneId: String
+    ): String? {
+        return decisionEvaluation.evaluationTime.let {
+            ResolverExtension.timestampToString(
+                it,
+                zoneId
+            )
         }
     }
 
